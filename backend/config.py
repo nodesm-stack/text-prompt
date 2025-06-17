d6f1a6c95a802ad44bd03f18ca999f4241e744f6
shivam <shivambmishra10@gmail.com>
2025-06-17 23:00:20 +0530

    updated code

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 # config.py
 
-API_KEY = "AIzaSyBpE2MKmZRFM4ZEpVBsDWCN5IeatWYsBxw"
+API_KEY = "AIzaSyDu2EGSYqD5nmhLgddRGPLReU_TyEL5TE0"
 MODEL_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
